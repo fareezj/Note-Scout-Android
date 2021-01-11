31d4d7239e8660faf4afb13fa4add781dd5b6ee9
Fareez <fareez.mohamad@hexainitiative.com>
2021-01-11 23:56:02 +0800

    Update delete icon

--- app/src/main/java/com/wolf/notescout/util/SwipeToDeleteCallback.kt
+++ app/src/main/java/com/wolf/notescout/util/SwipeToDeleteCallback.kt
@@ -15,7 +15,7 @@ import com.wolf.notescout.R
 
 abstract class SwipeToDeleteCallback(context: Context) : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
 
-    private val deleteIcon = ContextCompat.getDrawable(context, R.drawable.ic_baseline_add_24)
+    private val deleteIcon = ContextCompat.getDrawable(context, R.drawable.ic_baseline_delete_sweep_24)
     private val intrinsicWidth = deleteIcon?.intrinsicWidth
     private val intrinsicHeight = deleteIcon?.intrinsicHeight
     private val background = ColorDrawable()
